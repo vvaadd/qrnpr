aa1b4ac29d3392f3fb010bb3f8a00ee6409b2e74
vadim <rybinvv@gmail.com>
2014-11-13 00:11:38 +0300

    qrrest

---
+++ qrrest/src/main/java/ru/qrnpr/qrrest/service/RestService.java
@@ -0,0 +1,7 @@
+package ru.qrnpr.qrrest.service;
+
+/**
+ * Created by vadim on 13.11.14.
+ */
+public class RestService {
+}
---
+++ qrweb/src/main/java/ru/qrnpr/qrweb/api/MyResource.java
@@ -0,0 +1,30 @@
+package ru.qrnpr.qrweb.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("myresource")
+public class MyResource {
+    private static final Logger LOG = LoggerFactory.getLogger(MyResource.class);
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        LOG.debug("Method GET. Path = myresource");
+        return "Got it!";
+    }
+}
