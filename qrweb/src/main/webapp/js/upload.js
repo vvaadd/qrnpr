3b1e2c69124cb1cbc95b9ba8120b27f89ed642b2
Vadim <vrybin1987@mail.ru>
2015-04-03 01:10:23 +0300

    canvas

--- qrweb/src/main/webapp/js/upload.js
+++ qrweb/src/main/webapp/js/upload.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
     var uploadObject = $("#fileuploader").uploadFile({
         url: "rest/files/upload/",
         fileName: "file",
+        dragdropWidth: 100,
+        allowedTypes: "png",
+        acceptFiles: "image/",
+        showStatusAfterSuccess: false,
         multiple: false,
         onSuccess: function (files, data, xhr, pd) {
             //var obj = jQuery.parseJSON(data);
